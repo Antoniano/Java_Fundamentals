8022190e55c7b9e8a3a52f507f3150acc4456dda
Vadim Chakryan <vadim.romeo@gmail.com>
2013-02-26 23:53:24 +0200

    MiddleLevelThirdTaskMade

--- SecondPractice/src/ua/org/oa/alpharomeo/MiddleLevelThirdTask.java
+++ SecondPractice/src/ua/org/oa/alpharomeo/MiddleLevelThirdTask.java
@@ -2,20 +2,31 @@ package ua.org.oa.alpharomeo;
 
 public class MiddleLevelThirdTask {
 
+	/*
+	 * The program should contain 2 exceptions for 0 and very the last delimiter signs
+	 */
 
 	public static void main(String[] args) {
 
-		String test = "This is hor*osho!";
+		String test = "Thi*s i*s hor*osho!";
 		String delimiter = "*";
 		String result = "";
 		int length = test.length();
+		int count = 0;
 		
 		for (int i=0; i < length; i++) {
-			if ((test.charAt(i) != delimiter.charAt(0)) && (test.charAt(i+1) != delimiter.charAt(0))) {
-				result = result.sub;
-			}
+			if (test.charAt(i) == delimiter.charAt(0)) {count++;}
 		}
-			
+		
+		int start = 0; int end = 1;
+		for (int i=0; i < count; i++)  {
+			end = test.indexOf(delimiter, start);
+			result = result.concat(test.substring(start, end-1)); 
+			start = end+2;
+			System.out.println(length+ " " +end + " " + start);
+		}
+		result = result.concat(test.substring(start, test.length()));
+		System.out.println(result);
 		
 		
 	}
